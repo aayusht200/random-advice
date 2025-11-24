c1df87e62595913946ec12dc510d380dd7cd0287
aayusht200 <aayush.t200@gmail.com>
2025-11-24 11:26:52 +0530

    lintedCode

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import './style.css';
-import Typed from 'typed.js';
+import "./style.css"
+import Typed from "typed.js"
 
-const refreshAdvice = document.querySelector('.refresh');
+const refreshAdvice = document.querySelector(".refresh")
 async function getAdvice() {
-    const res = await fetch('https://api.adviceslip.com/advice');
-    const data = await res.json();
-    return data.slip.advice;
+	const res = await fetch("https://api.adviceslip.com/advice")
+	const data = await res.json()
+	return data.slip.advice
 }
-let typed = null;
+let typed = null
 async function init() {
-    const advice = await getAdvice();
-    if (typed) typed.destroy();
-    typed = new Typed('.advice', {
-        strings: [advice],
-        typeSpeed: 60,
-        showCursor: false,
-    });
+	const advice = await getAdvice()
+	if (typed) typed.destroy()
+	typed = new Typed(".advice", {
+		strings: [advice],
+		typeSpeed: 60,
+		showCursor: false,
+	})
 }
 
-init();
-setInterval(init, 20000);
-refreshAdvice.addEventListener('click', () => {
-    init();
-});
+init()
+setInterval(init, 20000)
+refreshAdvice.addEventListener("click", () => {
+	init()
+})
